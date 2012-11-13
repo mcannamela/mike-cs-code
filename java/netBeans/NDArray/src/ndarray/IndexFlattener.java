07cc7322fcac1109d893d252d8df2cf3478d9039
m.cannamela@gmail.com <m.cannamela@gmail.com@75a4d99d-4eed-fc33-49df-19e75944a8bf>
2012-11-13 18:00:41 +0000

    broke out index flattening into its own class

---
+++ java/netBeans/NDArray/src/ndarray/IndexFlattener.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ndarray;
+
+/**
+ *
+ * @author Michael
+ */
+public interface IndexFlattener {
+    int nDTo1D(int[] nDIndex);
+}
